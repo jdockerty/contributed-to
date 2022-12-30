dcf27ff76b5ddc304da06ac3756b53307946f6ba
Jack <56563911+jdockerty@users.noreply.github.com>
2022-12-30 19:04:50 +0000

    refactor: use graphql github api (#1)
    * update deps
    
    * swap to graphql for simpler data retrieval

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/jdockerty/contributed-to
 
 go 1.19
 
-require github.com/google/go-github/v48 v48.2.0
+require (
+	github.com/shurcooL/githubv4 v0.0.0-20221229060216-a8d4a561cc93
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+)
 
 require (
-	github.com/google/go-querystring v1.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/shurcooL/graphql v0.0.0-20220606043923-3cf50f8a0a29 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 )
--- main.go
+++ main.go
@@ -3,39 +3,81 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
-	"github.com/google/go-github/v48/github"
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	client := github.NewClient(nil)
 
-	var PullReqEvents []*github.Event
-	opt := &github.ListOptions{}
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GH_TOKEN_CONTRIBUTED_TO")},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+	client := githubv4.NewClient(httpClient)
+
+	v := map[string]interface{}{
+		"name":           githubv4.String("jdockerty"),
+		"mergedPRCursor": (*githubv4.String)(nil),
+	}
+
+	type Repository struct {
+		NameWithOwner string
+		Owner         struct {
+			Login string
+		}
+	}
+	type Info struct {
+		Owner string
+		URL   string
+	}
+	var query struct {
+		User struct {
+			PullRequests struct {
+				Nodes []struct {
+					Permalink  string
+					Repository struct {
+						NameWithOwner string
+						Owner         struct {
+							Login string
+						}
+					}
+				}
+				PageInfo struct {
+					EndCursor   githubv4.String
+					HasNextPage bool
+				}
+			} `graphql:"pullRequests(first: 100, after: $mergedPRCursor, states: MERGED)"`
+		} `graphql:"user(login: $name)"`
+	}
+
+	var allRepos []Info
 	for {
-		events, res, err := client.Activity.ListEventsPerformedByUser(context.Background(), "jdockerty", true, opt)
-		if err != nil {
+		if err := client.Query(context.Background(), &query, v); err != nil {
 			fmt.Println(err)
 			return
 		}
 
-		for _, event := range events {
-			if event.GetType() != "PullRequestEvent" {
+		for _, repo := range query.User.PullRequests.Nodes {
+			if repo.Repository.Owner.Login == "jdockerty" {
 				continue
 			}
-			PullReqEvents = append(PullReqEvents, event)
+			info := Info{
+				Owner: repo.Repository.Owner.Login,
+				URL:   repo.Permalink,
+			}
+			allRepos = append(allRepos, info)
 		}
 
-		if res.NextPage == 0 {
+		if !query.User.PullRequests.PageInfo.HasNextPage {
 			break
 		}
-
-		opt.Page = res.NextPage
-
+		v["mergedPRCursor"] = githubv4.String(query.User.PullRequests.PageInfo.EndCursor)
 	}
 
-	for _, event := range PullReqEvents {
-		fmt.Println(event.Repo.GetName())
+	for _, r := range allRepos {
+		fmt.Println(r.Owner, r.URL)
 	}
 
 }
