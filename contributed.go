780da80c8611ed07d4b10ba4854a7108faeb61cb
Jack <56563911+jdockerty@users.noreply.github.com>
2023-01-03 21:02:37 +0000

    feat: group by repo in response (#7)
    * update cache creation generic to MergedPullRequestInfo struct
    
    * add title to response info
    
    * update MergedPullRequestInfo type to map[string]struct
    
    * update graphql fetch to build grouped by owner/org info
    
    * update cache creation to new map type
    
    * update readme
    
    * update readme to include caching implementation
    
    * update response to order by owner/org and then repository as nested
    
    * add example structure for pull request

--- contributed.go
+++ contributed.go
@@ -22,10 +22,11 @@ var (
 		User struct {
 			PullRequests struct {
 				Nodes []struct {
+					Title      string
 					Permalink  string
 					Repository struct {
-						NameWithOwner string
-						Owner         struct {
+						Name  string
+						Owner struct {
 							Login     string
 							AvatarURL string
 						}
@@ -45,18 +46,53 @@ var (
 
 // MergedPullRequestInfo contains the relevant information which is fetched from
 // the GraphQL query, this is returned to the user.
-type MergedPullRequestInfo struct {
-	RepositoryOwner string
-	PullRequestURL  string
-	OwnerIconURL    string
+type MergedPullRequestInfo map[string]PullRequestInfo
+
+// PullRequestInfo represents information about a pull request to a repository
+// owner mapping. This holds the avatar URL and merged pull requests together.
+type PullRequestInfo struct {
+
+	// AvatarURL is the display picture of the repository owner or organisation.
+	AvatarURL string `json:"avatarURL"`
+
+	// PullRequests is the internal representation for the map structure of a
+	// owner/organisation to multiple merged pull requests.
+	PullRequests PullRequests `json:"pullRequests"`
 }
+
+// PullRequests is a custom wrapper around the structure of the response, this
+// is a mapping of the repository owner/organisation to the repositories that
+// they own, with the merged pull requests contained within the mapping.
+//
+// For example:
+//
+//	{
+//	  "google": {
+//	    "avatarURL": "...",
+//	    "pullRequests": {
+//	      "go-jsonnet": {
+//	        "PR Title 1": "URL 1",
+//	        "PR Title 2": "URL 2"
+//	      }
+//	    }
+//	  },
+//	  "hashicorp": {
+//	    "avatarURL": "...",
+//	    "pullRequests": {
+//	      "nomad": {
+//	        "PR Title 1": " URL 1"
+//	      }
+//	    }
+//	  }
+//	}
+type PullRequests map[string]map[string]string
 
 // fetchMergedPullRequestsByUser will fetch the merged pull requests for a given
 // user from the GitHub API, it initially uses a nil cursor that is then populated
 // from recurring calls.
-func fetchMergedPullRequestsByUser(ctx context.Context, client *githubv4.Client, name string, variables map[string]interface{}) ([]MergedPullRequestInfo, error) {
+func fetchMergedPullRequestsByUser(ctx context.Context, client *githubv4.Client, name string, variables map[string]interface{}) (map[string]PullRequestInfo, error) {
 
-	var allMergedPRs []MergedPullRequestInfo
+	mergedPRInfo := make(MergedPullRequestInfo)
 
 	for {
 		if err := client.Query(context.Background(), &query, variables); err != nil {
@@ -70,23 +106,36 @@ func fetchMergedPullRequestsByUser(ctx context.Context, client *githubv4.Client,
 				continue
 			}
 
-			mergedPR := MergedPullRequestInfo{
-				RepositoryOwner: node.Repository.NameWithOwner,
-				PullRequestURL:  node.Permalink,
-				OwnerIconURL:    node.Repository.Owner.AvatarURL,
+			initPRMap := make(map[string]map[string]string)
+			initMap := make(map[string]string)
+			// Initialise structure for repository owner to merged requests
+			// mapping.
+			if _, ok := mergedPRInfo[node.Repository.Owner.Login]; !ok {
+				mergedPRInfo[node.Repository.Owner.Login] = PullRequestInfo{
+					AvatarURL:    node.Repository.Owner.AvatarURL,
+					PullRequests: initPRMap,
+				}
 			}
 
-			allMergedPRs = append(allMergedPRs, mergedPR)
+			pullRequests := mergedPRInfo[node.Repository.Owner.Login].PullRequests
+			if _, ok := pullRequests[node.Repository.Name]; !ok {
+				pullRequests[node.Repository.Name] = initMap
+			}
+
+			pullRequests[node.Repository.Name][node.Title] = node.Permalink
+
 		}
 
+		// No more pull requests available
 		if !query.User.PullRequests.PageInfo.HasNextPage {
 			break
 		}
 
+		// Update cursor for next page
 		variables["mergedPRCursor"] = githubv4.String(query.User.PullRequests.PageInfo.EndCursor)
 	}
 
-	return allMergedPRs, nil
+	return mergedPRInfo, nil
 }
 
 // getGitHubClient wraps the creation of a GitHub GraphQL client.
@@ -120,7 +169,7 @@ func main() {
 	// each process or container. If there is a requirement later on, we can
 	// likely move to Redis in order to have a shared cache between multiple
 	// instances of the application.
-	cache, err := lru.New[string, interface{}](cacheSize)
+	cache, err := lru.New[string, MergedPullRequestInfo](cacheSize)
 	if err != nil {
 		fmt.Printf("unable to create cache: %s\n", err)
 		return
