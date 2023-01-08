e7b16e58c6db4806f609dba8efb5ec828f294433
Jack <56563911+jdockerty@users.noreply.github.com>
2023-01-08 20:56:47 +0000

    feat: simple cli (#10)
    * initial cli commit
    
    * move main functionality to pkg for use in CLI
    
    * add cli
    
    * add pkg/contributed for main functionality
    
    * update readme for correct public endpoint
    
    * update cli usage
    
    * update cli flag info

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ An application which displays contributions, i.e. merged pull requests, that a u
 
 ## Usage
 
-You can make a HTTP request yourself to `api.contributed-to.jdocklabs.co.uk/user/<user>` to receive a JSON payload of information about a particular user.
+You can make a HTTP request yourself to `api.contributed.jdocklabs.co.uk/user/<user>` to receive a JSON payload of information about a particular user.
 
 
 ### CLI
@@ -15,8 +15,19 @@ A small CLI is also available which can be installed using
 go install github.com/jdockerty/contributed-to/cmd/contributed@latest
 ```
 
-By default, this points to `api.contributed-to.jdocklabs.co.uk` but can be altered using the `address` flag if you wish to host the application
+By default, this points to `api.contributed.jdocklabs.co.uk` but can be altered using the `url` flag if you wish to host the application
 on your own infrastructure with specific GitHub tokens.
+
+You can use the application by calling it like so
+
+```bash
+# Contributions by one or more users
+contributed userOne userTwo
+
+# Verbose contribution information which includes the pull request title
+# and permalink
+contributed -full userOne userTwo
+```
 
 ## How it works
 
--- contributed.go
+++ contributed.go
@@ -10,147 +10,14 @@ import (
 
 	"github.com/gin-gonic/gin"
 	lru "github.com/hashicorp/golang-lru/v2"
+	"github.com/jdockerty/contributed-to/pkg/contributed"
 	"github.com/shurcooL/githubv4"
-	"golang.org/x/oauth2"
 )
 
 var (
-
-	// The static GraphQL query which we need to use in order to fetch the relevant
-	// data from the GitHub API.
-	query struct {
-		User struct {
-			PullRequests struct {
-				Nodes []struct {
-					Title      string
-					Permalink  string
-					Repository struct {
-						Name  string
-						Owner struct {
-							Login     string
-							AvatarURL string
-						}
-					}
-				}
-				PageInfo struct {
-					EndCursor   githubv4.String
-					HasNextPage bool
-				}
-			} `graphql:"pullRequests(first: 100, after: $mergedPRCursor, states: MERGED)"`
-		} `graphql:"user(login: $name)"`
-	}
-
 	cacheSize int
 	port      string
 )
-
-// MergedPullRequestInfo contains the relevant information which is fetched from
-// the GraphQL query, this is returned to the user.
-type MergedPullRequestInfo map[string]PullRequestInfo
-
-// PullRequestInfo represents information about a pull request to a repository
-// owner mapping. This holds the avatar URL and merged pull requests together.
-type PullRequestInfo struct {
-
-	// AvatarURL is the display picture of the repository owner or organisation.
-	AvatarURL string `json:"avatarURL"`
-
-	// PullRequests is the internal representation for the map structure of a
-	// owner/organisation to multiple merged pull requests.
-	PullRequests PullRequests `json:"pullRequests"`
-}
-
-// PullRequests is a custom wrapper around the structure of the response, this
-// is a mapping of the repository owner/organisation to the repositories that
-// they own, with the merged pull requests contained within the mapping.
-//
-// For example:
-//
-//	{
-//	  "google": {
-//	    "avatarURL": "...",
-//	    "pullRequests": {
-//	      "go-jsonnet": {
-//	        "PR Title 1": "URL 1",
-//	        "PR Title 2": "URL 2"
-//	      }
-//	    }
-//	  },
-//	  "hashicorp": {
-//	    "avatarURL": "...",
-//	    "pullRequests": {
-//	      "nomad": {
-//	        "PR Title 1": " URL 1"
-//	      }
-//	    }
-//	  }
-//	}
-type PullRequests map[string]map[string]string
-
-// fetchMergedPullRequestsByUser will fetch the merged pull requests for a given
-// user from the GitHub API, it initially uses a nil cursor that is then populated
-// from recurring calls.
-func fetchMergedPullRequestsByUser(ctx context.Context, client *githubv4.Client, name string, variables map[string]interface{}) (map[string]PullRequestInfo, error) {
-
-	mergedPRInfo := make(MergedPullRequestInfo)
-
-	for {
-		if err := client.Query(context.Background(), &query, variables); err != nil {
-			fmt.Println(err)
-			return nil, err
-		}
-
-		for _, node := range query.User.PullRequests.Nodes {
-
-			if node.Repository.Owner.Login == name {
-				continue
-			}
-
-			initPRMap := make(map[string]map[string]string)
-			initMap := make(map[string]string)
-			// Initialise structure for repository owner to merged requests
-			// mapping.
-			if _, ok := mergedPRInfo[node.Repository.Owner.Login]; !ok {
-				mergedPRInfo[node.Repository.Owner.Login] = PullRequestInfo{
-					AvatarURL:    node.Repository.Owner.AvatarURL,
-					PullRequests: initPRMap,
-				}
-			}
-
-			pullRequests := mergedPRInfo[node.Repository.Owner.Login].PullRequests
-			if _, ok := pullRequests[node.Repository.Name]; !ok {
-				pullRequests[node.Repository.Name] = initMap
-			}
-
-			pullRequests[node.Repository.Name][node.Title] = node.Permalink
-
-		}
-
-		// No more pull requests available
-		if !query.User.PullRequests.PageInfo.HasNextPage {
-			break
-		}
-
-		// Update cursor for next page
-		variables["mergedPRCursor"] = githubv4.String(query.User.PullRequests.PageInfo.EndCursor)
-	}
-
-	return mergedPRInfo, nil
-}
-
-// getGitHubClient wraps the creation of a GitHub GraphQL client.
-func getGitHubClient(ctx context.Context, token string) *githubv4.Client {
-
-	src := oauth2.StaticTokenSource(
-		&oauth2.Token{
-			AccessToken: token,
-		},
-	)
-
-	c := oauth2.NewClient(ctx, src)
-
-	return githubv4.NewClient(c)
-}
 
 func main() {
 	flag.IntVar(&cacheSize, "cache-size", 10000, "number of items available to cache")
@@ -163,13 +30,13 @@ func main() {
 		return
 	}
 
-	client := getGitHubClient(context.Background(), githubToken)
+	client := contributed.GetGitHubClient(context.Background(), githubToken)
 
 	// In-memory cache creation, this will lock multiple running instances to
 	// each process or container. If there is a requirement later on, we can
 	// likely move to Redis in order to have a shared cache between multiple
 	// instances of the application.
-	cache, err := lru.New[string, MergedPullRequestInfo](cacheSize)
+	cache, err := lru.New[string, contributed.MergedPullRequestInfo](cacheSize)
 	if err != nil {
 		fmt.Printf("unable to create cache: %s\n", err)
 		return
@@ -207,7 +74,7 @@ func main() {
 			"mergedPRCursor": (*githubv4.String)(nil),
 		}
 
-		pullRequests, err := fetchMergedPullRequestsByUser(context.Background(), client, name, queryVariables)
+		pullRequests, err := contributed.FetchMergedPullRequestsByUser(context.Background(), client, name, queryVariables)
 		if err != nil {
 			msg := fmt.Sprintf("unable to fetch data for %s", name)
 			c.JSON(500, gin.H{
